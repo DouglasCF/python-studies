f7b518dc1673a2a4dd66320ad083e661db4b130a
Douglas Fornaro <douglasfornaro@hotmail.com>
2021-01-24 13:52:50 -0300

    password-generator typo

--- password-generator.py
+++ password-generator.py
@@ -1,12 +1,12 @@
 import random
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
-symbols = "[]{}()*;/,_-!@#$%&+="
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+symbols = '[]{}()*;/,_-!@#$%&+='
 
 all = lower + upper + numbers + symbols
 
-lenght = 16
-password = "".join(random.sample(all, lenght))
+length = 16
+password = ''.join(random.sample(all, length))
 print(password)
